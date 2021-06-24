6ccb8c120dd41a9256a9e996cbbea9737fac4174
MedadRufus <medadrufus@gmail.com>
2021-06-24 03:53:10 +0100

    Don't use blocking DelayMicroseconds

--- src/SparkFun_u-blox_GNSS_Arduino_Library.cpp
+++ src/SparkFun_u-blox_GNSS_Arduino_Library.cpp
@@ -3004,7 +3004,7 @@ sfe_ublox_status_e SFE_UBLOX_GNSS::waitForACKResponse(ubxPacket *outgoingUBX, ui
 
     } //checkUbloxInternal == true
 
-    delayMicroseconds(500);
+    delay(1);
   } //while (millis() - startTime < maxTime)
 
   // We have timed out...
@@ -3114,7 +3114,7 @@ sfe_ublox_status_e SFE_UBLOX_GNSS::waitForNoACKResponse(ubxPacket *outgoingUBX, 
       }
     }
 
-    delayMicroseconds(500);
+    delay(1);
   }
 
   if (_printDebug == true)
